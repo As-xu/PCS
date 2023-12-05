8fbf58db28e58d2da0e2b96b86892413b07c968a
xsls\xs <1756232088@qq.com>
2023-12-05 22:25:45 +0800

    tj

--- gunicron_conf.py
+++ gunicron_conf.py
@@ -1,19 +1,10 @@
-# 并行工作进程数
 workers = 6
-# 指定每个工作者的线程数
 threads = 3
-# 监听内网端口5000
 bind = '0.0.0.0:8080'
-# 设置守护进程,将进程交给supervisor管理
 daemon = True
-# 工作模式协程
 worker_class = 'gevent'
-# 设置最大并发量
 worker_connections = 2000
-# 设置进程文件目录
 pidfile = '/home/PCS/gunicorn.pid'
-# 设置访问日志和错误信息日志路径
 accesslog = '/home/PCS/log/gunicorn_acess.log'
 errorlog = '/home/PCS/log/gunicorn_error.log'
-# 设置日志记录水平
 loglevel = 'info'

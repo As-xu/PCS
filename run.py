830b026135bdbd9a132ece82ec9f0c85a0c7034c
xsls\xs <1756232088@qq.com>
2023-12-10 23:41:49 +0800

    tj

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from pcs import create_app
 from gevent.pywsgi import WSGIServer
-from gevent.pywsgi import LoggingLogAdapter
+from werkzeug.middleware.proxy_fix import ProxyFix
 import logging
 
 app = create_app()
+app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == '__main__':
     if app.env == 'development':
-        app.run()
+        app.run(host='127.0.0.1', port=5000)
     else:
         server = WSGIServer(app.config['SERVER_NAME'],
                             log=logging.getLogger('pcs'),
